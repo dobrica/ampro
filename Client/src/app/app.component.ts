4291ff2ea510d16a0a8cc7d96907bbcd97a41caa
dobrica <djdobrica@gmail.com>
2022-06-18 15:07:07 +0200

    added templates endpoint

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Client';
+  templatesServiceBaseUrl = "http://172.23.0.6";
+  getAllTemplatesEndpoint = "/api/TextTemplates/getall";
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getTemplates();
+  }
+
+  getTemplates() {
+    this.httpClient.get(this.templatesServiceBaseUrl.concat(this.getAllTemplatesEndpoint)).subscribe(data => {
+      console.log(data);  
+    });
+  }
+
 }
