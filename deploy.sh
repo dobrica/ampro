15b52f5b318d7bc508c6e0b4cbecc236e6a42332
dobrica <djdobrica@gmail.com>
2022-06-20 13:40:11 +0200

    updated deploy.sh with docker compose (v2)

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 # TextStatisticsService
 cd TextStatisticsService
 sudo docker build -t statisticsservice .
-sudo docker-compose run -d --name statisticsservice web
+sudo docker compose run -d --name statisticsservice web
 cd ..
 
 sudo docker image rm $(docker images -f "dangling=true" -q)
 
 # PostgreSQL - MessageDB
 cd PostgreSQLConfig
-sudo docker-compose run -d --name postgresqldb db
+sudo docker compose run -d --name postgresqldb db
 cd ..
 
 sudo docker image rm $(docker images -f "dangling=true" -q)
@@ -18,7 +18,7 @@ sudo docker image rm $(docker images -f "dangling=true" -q)
 # MongoDB - TemplatesDB
 cd MongoDBConfig
 sudo docker build -t mongodb .
-sudo docker-compose run -d --name mongodb mongo
+sudo docker compose run -d --name mongodb mongo
 sudo docker exec -d mongodb ./startup.sh
 cd ..
 
@@ -27,7 +27,7 @@ sudo docker image rm $(docker images -f "dangling=true" -q)
 # MessagesService
 cd MessagesService
 sudo docker build -t messagesservice .
-sudo docker-compose run -d --name messagesservice web2
+sudo docker compose run -d --name messagesservice web2
 # InitDB
 dotnet ef database update
 cd ..
@@ -37,7 +37,7 @@ sudo docker image rm $(docker images -f "dangling=true" -q)
 # TemplatesService
 cd TemplatesService
 sudo docker build -t templatesservice .
-sudo docker-compose run -d --name templatesservice web1
+sudo docker compose run -d --name templatesservice web1
 cd ..
 
 sudo docker image rm $(docker images -f "dangling=true" -q)
@@ -45,7 +45,7 @@ sudo docker image rm $(docker images -f "dangling=true" -q)
 # Angular - Client
 cd Client
 sudo docker build -t amcapp .
-sudo docker-compose run -d --name amcapp app
+sudo docker compose run -d --name amcapp app
 cd ..
 
 sudo docker image rm $(docker images -f "dangling=true" -q)
